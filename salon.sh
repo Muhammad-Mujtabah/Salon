afbb6b8586daad1db3418b956da57f7b29641ea6
Muhammad-Mujtabah <106849693+Muhammad-Mujtabah@users.noreply.github.com>
2025-05-16 03:02:24 +0100

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,64 +1,48 @@
 #!/bin/bash
 
-# Command to execute PostgreSQL queries
 PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
-# Print welcome message
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
+echo 'Welcome to My Salon, how can I help you?'
 
-# Function to display the services
-SERVICES_SALON() {
-    # Retrieve list of services
-    MY_SERVICES=$($PSQL "select service_id, name from services order by service_id")
+MY_SERVICES=$($PSQL "select service_id, name from services order by service_id")
+echo "$MY_SERVICES" | while read SERVICE_ID BAR NAME; do
+    echo "$SERVICE_ID) $NAME"
+done
 
-    # If not services
-    if [[ -z $MY_SERVICES ]]; then
-        MAIN_MENU "Sorry, we don't have any service available right now."
-    else
-        # Display the services menu
-        echo 'Welcome to My Salon, how can I help you?'
-        echo "$MY_SERVICES" | while read SERVICE_ID BAR NAME; do
-            echo "$SERVICE_ID) $NAME"
-        done
+read SERVICE_ID_SELECTED
 
-        read SERVICE_ID_SELECTED
+SERVICE_NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED")
 
-        SERVICE_NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED")
+while [[ -z $SERVICE_NAME ]]; do
+    echo -e "\nI could not find that service. What would you like today?"
+    echo "$MY_SERVICES" | while read SERVICE_ID BAR NAME; do
+        echo "$SERVICE_ID) $NAME"
+    done
+    read SERVICE_ID_SELECTED
+    SERVICE_NAME=$($PSQL "select name from services where service_id=$SERVICE_ID_SELECTED")
+done
 
-        if [[ -z $SERVICE_NAME ]]; then
-            SERVICE_MENU
-        else
-            # read phone number
-            echo -e "\nYour phone number?"
-            read CUSTOMER_PHONE
+echo -e "\nYour phone number?"
+read CUSTOMER_PHONE
 
-            CUSTOMER_NAME=$($PSQL "select name from customers where phone='$CUSTOMER_PHONE';")
+CUSTOMER_NAME=$($PSQL "select name from customers where phone='$CUSTOMER_PHONE';")
 
-            # If not customer
-            if [[ -z $CUSTOMER_NAME ]]; then
-                echo -e "\nYour name?"
-                read CUSTOMER_NAME
+if [[ -z $CUSTOMER_NAME ]]; then
+    echo -e "\nYour name?"
+    read CUSTOMER_NAME
 
-                # service time
-                echo -e "\nHow many times do you want this service?"
-                read SERVICE_TIME
+    echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+    read SERVICE_TIME
 
-                # Insert new customer
-                ADD_NEW_CUSTOMER=$($PSQL "INSERT INTO customers(phone,name) values('$CUSTOMER_PHONE','$CUSTOMER_NAME')")
+    ADD_NEW_CUSTOMER=$($PSQL "INSERT INTO customers(phone,name) values('$CUSTOMER_PHONE','$CUSTOMER_NAME')")
+    CUSTOMER_ID=$($PSQL "SELECT customer_id from customers where name='$CUSTOMER_NAME'")
+    ADD_NEW_APPOINTMENT=$($PSQL "INSERT INTO appointments(time, customer_id, service_id) values('$SERVICE_TIME','$CUSTOMER_ID','$SERVICE_ID_SELECTED')")
+else
+    echo -e "\nWhat time would you like your $SERVICE_NAME, $CUSTOMER_NAME?"
+    read SERVICE_TIME
+    CUSTOMER_ID=$($PSQL "SELECT customer_id from customers where phone='$CUSTOMER_PHONE'")
+    ADD_NEW_APPOINTMENT=$($PSQL "INSERT INTO appointments(time, customer_id, service_id) values('$SERVICE_TIME','$CUSTOMER_ID','$SERVICE_ID_SELECTED')")
+fi
 
-                CUSTOMER_ID=$($PSQL "SELECT customer_id from customers where name='$CUSTOMER_NAME'")
-
-                ADD_NEW_APPOINTMENT=$($PSQL "INSERT INTO appointments(time, customer_id, service_id) values('$SERVICE_TIME','$CUSTOMER_ID','$SERVICE_ID_SELECTED')")
-            else
-                # If the customer exists
-                echo -e "\nHow many times do you want this service?"
-                read SERVICE_TIME
-            fi
-
-            echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
-        fi
-    fi
-}
-
-# start
-SERVICES_SALON
+echo -e "\nI have put you down for a $SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
